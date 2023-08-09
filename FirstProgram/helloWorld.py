bbeee2fa9df42209c24a4f6611725987a4a8e882
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2023-08-09 11:30:22 +0530

    commit

---
+++ FirstProgram/helloWorld.py
@@ -0,0 +1,52 @@
+# print("hello world")
+
+# Variables
+a=2
+print(a);
+a="Hello"
+print(a)
+b=None
+print(b)
+b="Hello Devansh"
+print(b)
+
+c="5"
+print(int(c)+1)
+
+
+# operators
+
+d=4
+e=2
+print("the result for d/e is:",d/e)
+print("the result for d*e is:",d*e)
+print("the result for d**e is:",d**e)
+print("the result for d//e is:",d//e)
+print("the result for d%e is:",d%e)
+
+d**=2
+print(d)
+d//=2
+print(d)
+d%=2
+
+print(d)
+
+
+# logical operators
+
+print(True and False)
+print(d>e and d<e)
+print(d>e or d<e)
+
+
+# strings
+
+name="honey"
+
+print(name[0:3])
+print(name.upper())
+print(name)
+
+
+
---
+++ FirstProgram/user.py
@@ -0,0 +1,6 @@
+from new import greet
+import numpy as np
+
+greet("honey")
+a=np.array([1,2,3,4])
+print(a)
